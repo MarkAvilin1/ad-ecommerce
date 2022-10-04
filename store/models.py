beaca8b6bb84796b28b7b6ff9d5605c094bf28a9
markavilin1 <avilin1@outlook.com>
2022-10-04 20:59:43 -0300

    New update

--- store/models.py
+++ store/models.py
@@ -1,22 +1,22 @@
 from django.db import models
-from category.models import Category
-from django.urls import reverse
-from accounts.models import Account
 from django.db.models import Avg, Count
+from django.urls import reverse
 
-# Create your models here.
+from accounts.models import Account
+from category.models import Category
+
 
 class Product(models.Model):
-    product_name    = models.CharField(max_length=200, unique=True)
-    slug            = models.SlugField(max_length=200, unique=True)
-    description     = models.TextField(max_length=500, blank=True)
-    price           = models.IntegerField()
-    images          = models.ImageField(upload_to='photos/products')
-    stock           = models.IntegerField()
-    is_available    = models.BooleanField(default=True)
-    category        = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_date    = models.DateTimeField(auto_now_add=True)
-    modified_date   = models.DateTimeField(auto_now=True)
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.IntegerField()
+    images = models.ImageField(upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
 
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
@@ -38,6 +38,7 @@ class Product(models.Model):
             count = int(reviews['count'])
         return count
 
+
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager, self).filter(variation_category='color', is_active=True)
@@ -45,17 +46,19 @@ class VariationManager(models.Manager):
     def sizes(self):
         return super(VariationManager, self).filter(variation_category='size', is_active=True)
 
+
 variation_category_choice = (
     ('color', 'color'),
     ('size', 'size'),
 )
 
+
 class Variation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variation_category = models.CharField(max_length=100, choices=variation_category_choice)
-    variation_value     = models.CharField(max_length=100)
-    is_active           = models.BooleanField(default=True)
-    created_date        = models.DateTimeField(auto_now=True)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
 
     objects = VariationManager()
 
