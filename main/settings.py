13376844949f61c5f049d0ce5a302b2ed68acf75
markavilin1 <avilin1@outlook.com>
2022-10-04 00:22:51 -0300

    New update

--- main/settings.py
+++ main/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
-STATIC_HOST = "https://d4663kmspf1sqa.cloudfront.net" if not DEBUG else ""
-STATIC_URL = STATIC_HOST + "/static/"
-# STATIC_URL = 'static/'
+# STATIC_HOST = "https://d4663kmspf1sqa.cloudfront.net" if not DEBUG else ""
+# STATIC_URL = STATIC_HOST + "/static/"
+STATIC_URL = 'static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
