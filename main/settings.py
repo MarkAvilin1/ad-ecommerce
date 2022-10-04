c121440f6e8aabbb228df3ac20f2d3318d0fa96b
markavilin1 <avilin1@outlook.com>
2022-10-04 00:28:54 -0300

    New update

--- main/settings.py
+++ main/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (
 
 
 # media files configuration
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
